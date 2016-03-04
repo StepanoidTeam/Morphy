e2a75a74334762e82c03024acdd44ed201327975
bobanko <mishko.vladimir@gmail.com>
2016-03-04 03:04:08 +0200

    coins added, saw restyled, coin mechanics in progress

---
+++ Assets/Scripts/BlobMover.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlobMover : MonoBehaviour
+{
+
+	public float JumpForce = 10.0f;
+	public float Speed = 10.0f;
+
+	public int groundedBodies = 1;
+	public float onAirSpeedPenalty = 4f;
+
+	public LayerMask m_GroundLayer;
+
+	JellySprite m_JellySprite;
+
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start()
+	{
+		m_JellySprite = GetComponent<JellySprite>();
+	}
+
+	public float m_GroundRayOffset = 3.0f;
+	public float m_GroundRayLength = 3.0f;
+	
+
+	bool CheckIsGrounded()
+	{
+		return m_JellySprite.IsGrounded(m_GroundLayer, groundedBodies);
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		var isGrounded = CheckIsGrounded();
+
+		var axes = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		if (Input.GetButton("Jump") && isGrounded)
+		{
+			m_JellySprite.AddForce(JumpForce * Vector2.up);
+		}
+
+		m_JellySprite.AddForce(Vector2.right * axes.x * Speed * (isGrounded ? 1 : 1 / onAirSpeedPenalty));
+
+		if (!isGrounded && axes.z < 0)
+		{
+			m_JellySprite.AddForce(Vector2.up * axes.z * Speed);
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Destroyable.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Destroyable : MonoBehaviour
+{
+	public float Durability = 1f;
+	public float MaxDurability = 1f;
+	public Vector3 spawnPosition;
+
+	public float RespawnTimeout = 1f;
+
+
+	public string[] Hazards;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		spawnPosition = transform.position;
+
+	}
+
+
+	public void Hit(float hitRate)
+	{
+
+		Durability -= hitRate;
+
+		if (Durability <= 0)
+		{
+			StartCoroutine(Respawn());
+			
+		}
+
+	}
+
+	public IEnumerator Respawn()
+	{
+		gameObject.GetComponent<Renderer>().enabled = false;
+
+		yield return new WaitForSeconds(RespawnTimeout);
+
+		
+
+		this.Durability = MaxDurability;
+		//this.transform.position = spawnPosition;
+
+		//this.transform.position.Set(spawnPosition.x, spawnPosition.y,spawnPosition.z); ;
+		//this.transform.position.Set(70f, 25f, 0);
+
+		var jelly = this.GetComponent<JellySprite>();
+		jelly.SetPosition(spawnPosition, true);
+
+		gameObject.GetComponent<Renderer>().enabled = true;
+
+		Debug.Log("respawned " + Time.time);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+
+	void FixedUpdate()
+	{
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D trigger) {
+		if (Array.IndexOf(Hazards, trigger.tag) >= 0)
+		{
+			var go = trigger.gameObject;
+			var damager = go.GetComponent<Damager>();
+			if (damager)
+			{
+				Hit(damager.DamagePerSecond);
+			}
+		}
+	}
+
+
+	void OnJellyTriggerEnter2D(JellySprite.JellyCollider2D trigger)
+	{
+
+		if (Array.IndexOf(Hazards, trigger.Collider2D.tag) >= 0)
+		{
+			var go = trigger.Collider2D.gameObject;
+			var damager = go.GetComponent<Damager>();
+			if (damager)
+			{
+				Hit(damager.DamagePerSecond);
+			}
+		}
+	}
+
+}
