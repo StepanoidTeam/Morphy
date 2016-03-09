9a09bb6853f02a246cf1b1516df91b96d13b99d5
bobanko <mishko.vladimir@gmail.com>
2016-03-09 18:48:19 +0200

    coin refac, mace added (inprogress) gamemanager added - in progress

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+	public Camera m_Camera;
+
+	public Transform m_PlayerPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
