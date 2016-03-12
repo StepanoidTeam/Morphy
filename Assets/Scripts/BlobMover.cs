3510653ae3a5fbb613a0890b1ebb5265d26b1b9c
bobanko <mishko.vladimir@gmail.com>
2016-03-13 00:32:15 +0200

    morphy moving animations added, jumping in progress, minor cleanup

--- Assets/Scripts/BlobMover.cs
+++ Assets/Scripts/BlobMover.cs
@@ -14,6 +14,10 @@ public class BlobMover : MonoBehaviour
 
 	JellySprite m_JellySprite;
 
+	Animator m_Animator;
+
+	Rigidbody2D m_JellyRigidbody2D;
+
 
 	/// <summary>
 	/// Start this instance.
@@ -21,11 +25,14 @@ public class BlobMover : MonoBehaviour
 	void Start()
 	{
 		m_JellySprite = GetComponent<JellySprite>();
+
+		m_Animator = GetComponentInChildren<Animator>();
+		//m_JellyRigidbody2D = m_JellySprite.GetComponent<Rigidbody2D>();
 	}
 
 	public float m_GroundRayOffset = 3.0f;
 	public float m_GroundRayLength = 3.0f;
-	
+
 
 	bool CheckIsGrounded()
 	{
@@ -40,10 +47,19 @@ public class BlobMover : MonoBehaviour
 
 		var axes = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
+		Debug.Log(axes);
+
+		m_Animator.SetFloat("HDirection", axes.x);
+		//m_Animator.SetFloat("VDirection", m_JellyRigidbody2D.velocity.normalized.y);
+			
+
+
 		if (Input.GetButton("Jump") && isGrounded)
 		{
 			m_JellySprite.AddForce(JumpForce * Vector2.up);
 		}
+
+
 
 		m_JellySprite.AddForce(Vector2.right * axes.x * Speed * (isGrounded ? 1 : 1 / onAirSpeedPenalty));
 
