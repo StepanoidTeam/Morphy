4aac12341538e631dab68f0b83c7f08ee1ac8a0b
bobanko <mishko.vladimir@gmail.com>
2016-03-14 02:11:33 +0200

    blob state change refac - logic moved to mgr, minor refacs

---
+++ Assets/BlobStateIndicator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class BlobStateIndicator : MonoBehaviour
+{
+
+
+	public Sprite StateImage;
+
+	bool isActive;
+
+	public string Hotkey;
+
+	Color colorActive = new Color(1, 1, 1, 1);
+	Color colorInactive = new Color(1, 1, 1, 1);
+
+	Image m_Image;
+	Text m_Text;
+
+
+	public void Init(Texture2D tex, string hotkey){
+		StateImage = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height),Vector2.one);
+		isActive = false;
+		Hotkey = hotkey;
+	}
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+		m_Image = GetComponent<Image>();
+		m_Text = GetComponentInChildren<Text>();
+
+
+		m_Image.sprite = StateImage;
+		m_Text.text = Hotkey;
+
+		SetState(isActive);
+	}
+
+
+
+	public void SetState(bool value)
+	{
+		isActive = value;
+		//m_Image.material.SetColor("_Color", isActive ? colorActive : colorInactive);
+	}
+}
